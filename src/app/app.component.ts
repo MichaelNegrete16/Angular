c7b0a463b81c137ef2205cab623ecac08ceb4d4b
Maicol <michael.negrete16@gmail.com>
2022-07-05 16:48:13 -0500

    Primero pasos -> Interpolacion - Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'HolaMundo';
+  
+  // Interpolacion -> Conexion entre en componente y html
+  nombre = 'Juan'
+
+  constructor () {
+    setInterval( () => this.nombre = 'Javier' , 3000 )
+
+    setInterval(() => this.deshabilitado = false , 3000)
+  }
+
+  getSumar = (numero1: number ,numero2: number) => {
+      return numero1 + numero2
+  }
+
+  // Property Binding
+  textPlaceHolder = 'Escriba algo aqui..'
+  deshabilitado = true
+  imgSrc = 'https://i.ytimg.com/vi/qJxYoiFzIC0/maxresdefault.jpg'
+
+ 
+
 }
